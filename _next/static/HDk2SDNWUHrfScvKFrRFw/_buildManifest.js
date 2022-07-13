cc34590ac62889e81cc5cc992aea7b3ba6198cbf
fdograph <fdograph@gmail.com>
2022-07-13 19:21:30 +0000

    Deploying to gh-pages from @ fdograph/fancy@e6a21a16b2ddd59f6954bd93e2df97968feeb629 🚀

--- _next/static/HDk2SDNWUHrfScvKFrRFw/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/846e3e9b03a632b2.css","static/chunks/pages/index-e21e6730349c7a55.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
