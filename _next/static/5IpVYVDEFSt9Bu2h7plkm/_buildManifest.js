9cb5e040dc6a0a22d4edfabd6cff2dad22f13e93
fdograph <fdograph@gmail.com>
2022-07-18 19:52:05 +0000

    Deploying to gh-pages from @ fdograph/fancy@41d1637da5683452953646e1f321a7cdb5ef9b79 🚀

--- _next/static/5IpVYVDEFSt9Bu2h7plkm/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/e586be82d2ae5bda.css","static/chunks/pages/index-740c5c21711c91bb.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/LWo9ogZ6ZhyJMTsuOJZ8S/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/e586be82d2ae5bda.css","static/chunks/pages/index-7349ca9f69356b8b.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
